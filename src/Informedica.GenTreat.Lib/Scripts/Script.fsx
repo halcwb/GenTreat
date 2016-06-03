5713e31f4f232ba05ed6699898e6d8d91639fa26
Casper Bollen <halcwb@gmail.com>
2016-06-03 06:26:31 +0200

    Added first domain model

---
+++ src/Informedica.GenTreat.Lib/Scripts/Script.fsx
@@ -0,0 +1,20 @@
+﻿#load "load-project-release.fsx"
+
+
+module GenTreat =
+
+    type Patient = Patient
+
+    type Treatment = Treatment
+
+    type Target = Target
+
+    type PatientTreatment = PatientTreatment
+
+    type Condition = Condition
+
+    type Protocol =  Target * (Condition * Treatment list) list
+
+    type Treat = Treatment * Target * PatientTreatment -> Treatment
+
+    type Evaluate = Patient * PatientTreatment * Target list * Protocol list -> Treatment list
